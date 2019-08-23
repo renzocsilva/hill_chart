85ca1888bdb5ccd33059f583216277ca37adb660
Renzo Correa Silva <renzo@aimende.com>
2019-08-23 14:33:54 +0000

    get more y-axis room

---
+++ global.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(shiny)
+library(DT)
+library(ggrepel)
+
+
+### References:
+#The editable table's coding was mostly obtained from Yihui Xie's answer in the post below
+#https://groups.google.com/forum/#!topic/shiny-discuss/FeqU0AoTpz0
+
+### Initial fixed parameters #TODO: consider taking shiny input here.
+u <- 50
+sd <- 20
+base <-
+  data.frame(x = seq(0, 100, length = 100),
+             y = dnorm(seq(0, 100, length = 100), mean = u, sd = sd))
+
+### Useful
+
+
+# # Print the values of inputs
+# output$x2 = renderPrint({
+#   data.frame(Task = shinyValue('v1_', 20), Progress = shinyValue('v2_', 20), Show = shinyValue('v3_',20))
+# })
+#verbatimTextOutput('x2')
